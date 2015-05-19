6282a4a1f8c904a3dd9c6889f40b64cf761e83a0
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-05-19 18:26:11 +0200

    Bump version

--- stock_shipment_management/__openerp__.py
+++ stock_shipment_management/__openerp__.py
@@ -19,7 +19,7 @@
 #
 #
 {"name": "Shipment Management",
- "version": "1.0",
+ "version": "1.0.1",
  "author": "Camptocamp,Odoo Community Association (OCA)",
  "category": "Transportation",
  "license": 'AGPL-3',
