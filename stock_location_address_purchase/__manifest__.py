ef2cb2cc8bd19cc2c96447eb84489e9711cd8199
Enric Tobella <etobella@creublanca.es>
2018-03-26 12:00:21 +0200

    [ADD] stock_location_address_purchase

---
+++ stock_location_address_purchase/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Creu Blanca
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Purchase Location address',
+    'summary': 'Uses the location address on purchases',
+    'version': '11.0.1.0.0',
+    'license': 'LGPL-3',
+    'website': 'https://github.com/purchase-workflow',
+    'author': 'Creu Blanca, '
+              'Odoo Community Association (OCA)',
+    'category': 'Purchase Management',
+    'depends': [
+        'purchase',
+        'stock_location_address',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
