222d953e9cc9c96d62c22d307aff34de385a8c4c
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-09 18:06:48 +0100

    [IMP] stock_location_address: black, isort

--- stock_location_address/tests/test_location_address.py
+++ stock_location_address/tests/test_location_address.py
@@ -6,22 +6,14 @@ from odoo.tests import TransactionCase
 
 class TestLocationAddress(TransactionCase):
     def test_inheritance(self):
-        partner_1 = self.env['res.partner'].create({
-            'name': 'Partner1'
-        })
-        partner_2 = self.env['res.partner'].create({
-            'name': 'Partner2'
-        })
-        parent_location = self.env['stock.location'].create({
-            'name': 'Parent',
-            'usage': 'internal',
-            'address_id': partner_1.id
-        })
-        location = self.env['stock.location'].create({
-            'name': 'Location',
-            'usage': 'internal',
-            'location_id': parent_location.id,
-        })
+        partner_1 = self.env["res.partner"].create({"name": "Partner1"})
+        partner_2 = self.env["res.partner"].create({"name": "Partner2"})
+        parent_location = self.env["stock.location"].create(
+            {"name": "Parent", "usage": "internal", "address_id": partner_1.id}
+        )
+        location = self.env["stock.location"].create(
+            {"name": "Location", "usage": "internal", "location_id": parent_location.id}
+        )
         self.assertEqual(location.real_address_id, partner_1)
         location.address_id = partner_2
         self.assertEqual(location.real_address_id, partner_2)
