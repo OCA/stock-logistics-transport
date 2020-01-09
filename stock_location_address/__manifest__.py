45f99810bc90e7bb606de15af6e716ec51e0d3a2
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-09 18:06:48 +0100

    [IMP] stock_location_address: black, isort

--- stock_location_address/__manifest__.py
+++ stock_location_address/__manifest__.py
@@ -2,20 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Stock Location address',
-    'summary': 'Adds an address on locations',
-    'version': '12.0.1.0.0',
-    'author': 'Creu Blanca, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/stock-logistics-warehouse',
-    'category': 'Warehouse Management',
-    'depends': [
-        'stock',
-    ],
-    'data': [
-        'views/stock_location_views.xml',
-    ],
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
+    "name": "Stock Location address",
+    "summary": "Adds an address on locations",
+    "version": "12.0.1.0.0",
+    "author": "Creu Blanca, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse Management",
+    "depends": ["stock"],
+    "data": ["views/stock_location_views.xml"],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
 }
