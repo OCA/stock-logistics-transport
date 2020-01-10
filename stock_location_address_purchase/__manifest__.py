0f48f548924c052ddc085210b1b9e7dced4f2bff
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-10 11:00:31 +0100

    [IMP] stock_location_address_purchase: black, isort

--- stock_location_address_purchase/__manifest__.py
+++ stock_location_address_purchase/__manifest__.py
@@ -2,17 +2,13 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Purchase Location address',
-    'summary': 'Uses the location address on purchases',
-    'version': '12.0.1.0.0',
-    'license': 'LGPL-3',
-    'website': 'https://github.com/purchase-workflow',
-    'author': 'Creu Blanca, '
-              'Odoo Community Association (OCA)',
-    'category': 'Purchases',
-    'depends': [
-        'purchase',
-        'stock_location_address',
-    ],
-    'installable': True,
+    "name": "Purchase Location address",
+    "summary": "Uses the location address on purchases",
+    "version": "12.0.1.0.0",
+    "license": "LGPL-3",
+    "website": "https://github.com/purchase-workflow",
+    "author": "Creu Blanca, " "Odoo Community Association (OCA)",
+    "category": "Purchases",
+    "depends": ["purchase", "stock_location_address"],
+    "installable": True,
 }
