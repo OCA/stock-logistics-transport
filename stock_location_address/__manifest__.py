90aa101fc22dafb836b8f0c34b0f12cf9a4ea7a4
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2021-12-20 09:13:21 +0100

    [15.0][MIG] stock_location_address: Migration to 15.0

--- stock_location_address/__manifest__.py
+++ stock_location_address/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Location address",
     "summary": "Adds an address on locations",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Creu Blanca, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-transport",
     "category": "Warehouse Management",
