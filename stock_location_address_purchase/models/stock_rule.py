0e02952db048962f0d3dccd0f6acebd30276b53e
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2021-12-23 09:16:47 +0100

    [MIG] stock_location_address_purchase: Migration to 15.0

--- stock_location_address_purchase/models/stock_rule.py
+++ stock_location_address_purchase/models/stock_rule.py
@@ -1,5 +1,5 @@
 # Copyright 2021 Creu Blanca
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models
 
