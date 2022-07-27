b710dd3a1ff6d18e5f7235abf0d6c64aad9261cc
Niklas Tom Hucke <nitohu@gmail.com>
2022-07-27 11:53:41 +0200

    [ADD] TypeError test for _create_make_procurement function

--- stock_location_address_purchase/__manifest__.py
+++ stock_location_address_purchase/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Purchase Location address",
     "summary": "Uses the location address on purchases",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/stock-logistics-transport",
     "author": "Creu Blanca, Odoo Community Association (OCA)",
--- stock_location_address_purchase/tests/test_purchase_buy_rule.py
+++ stock_location_address_purchase/tests/test_purchase_buy_rule.py
@@ -10,7 +10,9 @@ class TestPurchaseBuyRule(PurchaseTestCommon):
         company = self.env.ref("base.main_company")
         # Update company with Purchase Lead Time
         company.write({"po_lead": 3.00})
-        date_planned = fields.datetime.now() + timedelta(days=10)
+        date_planned = fields.Datetime.now() + timedelta(days=10)
+        # Prepare string from timestamp
+        date_planned_str = fields.Datetime.to_string(date_planned)
         # This function takes a date as argument
         self._create_make_procurement(self.product_1, 15.00, date_planned=date_planned)
         purchase = (
@@ -20,12 +22,15 @@ class TestPurchaseBuyRule(PurchaseTestCommon):
         )
         self.assertTrue(purchase)
         self.assertFalse(purchase.dest_address_id)
+        # Test for TypeError when passing date as string
+        self.assertRaises(TypeError, self._create_make_procurement(self.product_1, 15.00, date_planned=date_planned_str))
 
     def test_destination(self):
         company = self.env.ref("base.main_company")
         # Update company with Purchase Lead Time
         company.write({"po_lead": 3.00})
-        date_planned = fields.datetime.now() + timedelta(days=10)
+        date_planned = fields.Datetime.now() + timedelta(days=10)
+        date_planned_str = fields.Datetime.to_string(date_planned)
         partner = self.env["res.partner"].create({"name": "DEMO Partner"})
         self.warehouse_1.lot_stock_id.real_address_id = partner
         self._create_make_procurement(self.product_1, 15.00, date_planned=date_planned)
@@ -37,3 +42,4 @@ class TestPurchaseBuyRule(PurchaseTestCommon):
         self.assertTrue(purchase)
         self.assertTrue(purchase.dest_address_id)
         self.assertEqual(purchase.dest_address_id, partner)
+        self.assertRaises(TypeError, self._create_make_procurement(self.product_1, 15.00, date_planned=date_planned_str))
