e04ba7db6af6182ebed33954d9290cbafa69cf92
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-12-14 10:48:01 +0100

    [MIG] stock_dock: Migration to 16.0

--- stock_dock/__manifest__.py
+++ stock_dock/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Loading Dock",
     "summary": "Manage the loading docks of your warehouse.",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-transport",
     "category": "Warehouse Management",
