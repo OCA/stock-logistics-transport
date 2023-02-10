0ef17ecda9ab9984c3802b21accde1b5d3b30d27
sbejaoui <souheil.bejaoui@acsone.eu>
2023-02-10 17:55:22 +0100

    [ADD] shipment_advice_planner
    This module is used to plan ready transfers in shipment advices.
    
    As this is the base module, it provides only a simple planning mode.
    Transfers are grouped to have a single shipment advice by warehouse.
    
    [IMP] shipment_advice_planner: Add init script
    
    [FIX] - shipment_advice_planner: fix typo
    
    [FIX] - group pickings to plan by picking type instead of the warehouse
    
    [IMP] - shipment_advice_planner: add search for can_be_planned_in_shipment_advice field
    
    [IMP] shipment_advice_planner: shipment advice is created confirmed and arrival and departure date
    
    [IMP] - shipment_advice_planner: add unit tests
    
    [IMP] shipment_advice_planner: add author & maintainer
    
    Co-authored-by: Jacques-Etienne Baudoux <je@bcim.be>
    
    [IMP] add depends_context to _compute_picking_to_plan_ids

---
+++ shipment_advice_planner/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shipment Advice Planner",
+    "summary": """This module is used to plan ready transfers in shipment advices.""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,BCIM,Odoo Community Association (OCA)",
+    "maintainers": ["jbaudoux"],
+    "website": "https://github.com/OCA/stock-logistics-transport",
+    "depends": ["shipment_advice"],
+    "data": [
+        "security/shipment_advice_planner.xml",
+        "wizards/shipment_advice_planner.xml",
+    ],
+    "demo": [],
+    "pre_init_hook": "pre_init_hook",
+}
---
+++ shipment_advice_planner/tests/__init__.py
@@ -0,0 +1,2 @@
+from . import test_picking_can_be_planned
+from . import test_shipment_advice_planner
