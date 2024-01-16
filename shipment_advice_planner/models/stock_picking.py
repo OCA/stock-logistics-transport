77913c78928931797009d7406a991b72ae6a21de
sbejaoui <souheil.bejaoui@acsone.eu>
2024-01-16 16:58:06 +0100

    [IMP] picking can be planned if shipment advice in error

--- shipment_advice_planner/models/stock_picking.py
+++ shipment_advice_planner/models/stock_picking.py
@@ -14,13 +14,21 @@ class StockPicking(models.Model):
 
     @api.model
     def _get_compute_picking_to_plan_ids_depends(self):
-        return ["planned_shipment_advice_id", "state", "picking_type_code"]
+        return [
+            "planned_shipment_advice_id",
+            "planned_shipment_advice_id.state",
+            "state",
+            "picking_type_code",
+        ]
 
     @api.depends(lambda m: m._get_compute_picking_to_plan_ids_depends())
     def _compute_can_be_planned_in_shipment_advice(self):
         for rec in self:
             rec.can_be_planned_in_shipment_advice = (
-                not rec.planned_shipment_advice_id
+                (
+                    not rec.planned_shipment_advice_id
+                    or rec.planned_shipment_advice_id.state == "error"
+                )
                 and rec.state == "assigned"
                 and rec.picking_type_code == "outgoing"
             )
@@ -28,14 +36,18 @@ class StockPicking(models.Model):
     def _search_can_be_planned_in_shipment_advice(self, operator, value):
         if (operator == "=" and value) or (operator == "!=" and not value):
             return [
+                "|",
                 ("planned_shipment_advice_id", "=", False),
+                ("planned_shipment_advice_id.state", "=", "error"),
                 ("state", "=", "assigned"),
                 ("picking_type_code", "=", "outgoing"),
             ]
         return [
             "|",
             "|",
+            "|",
             ("planned_shipment_advice_id", "!=", False),
+            ("planned_shipment_advice_id.state", "!=", "error"),
             ("state", "!=", "assigned"),
             ("picking_type_code", "!=", "outgoing"),
         ]
