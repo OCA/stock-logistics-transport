e1e59cb4ae06fe7142c8c3c0e054ff65b38767c4
Thierry Ducrest <thierry@ducrest.net>
2024-04-16 09:19:19 +0200

    Add shipment_advice_bill_auto_complete_mrp

---
+++ shipment_advice_bill_auto_complete_mrp/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Shipment Advice Invoice Auto Complete MRP",
+    "summary": "Glue module between Shipment Advice Bill Auto Complete and MRP",
+    "version": "14.0.1.0.0",
+    "category": "Invoicing Management",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["TDu"],
+    "license": "AGPL-3",
+    "depends": [
+        "shipment_advice_bill_auto_complete",
+        "purchase_mrp",
+    ],
+    "website": "https://github.com/OCA/stock-logistics-transport",
+    "auto_install": True,
+    "installable": True,
+}
