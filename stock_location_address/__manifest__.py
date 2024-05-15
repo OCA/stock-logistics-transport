85059e29ae71991766c6b398406ad29a56373c62
Mikheil Barnaveli <barnaveli@hmg.software>
2024-05-15 13:49:16 +0400

    [MIG] stock_location_address: Migration to 16.0

--- stock_location_address/__manifest__.py
+++ stock_location_address/__manifest__.py
@@ -4,12 +4,16 @@
 {
     "name": "Stock Location address",
     "summary": "Adds an address on locations",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Creu Blanca, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-transport",
     "category": "Warehouse Management",
-    "depends": ["stock"],
-    "data": ["views/stock_location_views.xml"],
+    "depends": [
+        "stock"
+    ],
+    "data": [
+        "views/stock_location_views.xml"
+    ],
     "license": "AGPL-3",
     "installable": True,
     "application": False,
