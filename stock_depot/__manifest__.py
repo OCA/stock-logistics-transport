f2ac3df46bc7d74f1b27a60b722ea40d11644e3b
Bhavesh Heliconia <oca@heliconia.in>
2025-04-14 16:55:36 +0530

    [MIG] stock_depot: Migration to 18.0

--- stock_depot/__manifest__.py
+++ stock_depot/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Depot",
     "summary": """This module allows users to manage partners stock depots.""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-transport",
