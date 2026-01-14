e1560845200f108997db2f34769af1f38bca1692
r.perez <r.perez@binhex.cloud>
2026-01-14 17:38:13 -0500

    [IMP] shipment_advice: support manual ordering for planned pickings in a shipment advice

--- shipment_advice/models/stock_move.py
+++ shipment_advice/models/stock_move.py
@@ -1,8 +1,7 @@
 # Copyright 2021 Camptocamp SA
 # Copyright 2024 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
@@ -15,6 +14,22 @@ class StockMove(models.Model):
         index=True,
         copy=False,
     )
+
+    picking_sequence_in_shipment_advice = fields.Integer(
+        compute="_compute_picking_sequence_in_shipment_advice"
+    )
+
+    @api.depends_context("active_shipment_advice_id")
+    def _compute_picking_sequence_in_shipment_advice(self):
+        shipment_advice_id = self.env.context.get("active_shipment_advice_id")
+
+        for move in self:
+            picking_sequence = False
+            if shipment_advice_id:
+                picking_sequence = move.picking_id.with_context(
+                    active_shipment_advice_id=shipment_advice_id
+                ).sequence_in_shipment_advice
+            move.picking_sequence_in_shipment_advice = picking_sequence
 
     def _plan_in_shipment(self, shipment_advice):
         """Plan the moves into the given shipment advice."""
@@ -34,4 +49,19 @@ class StockMove(models.Model):
     def _action_cancel(self):
         res = super()._action_cancel()
         self.shipment_advice_id.auto_close_incoming_shipment_advices()
+        return res
+
+    @api.model
+    def search(self, domain, offset=0, limit=None, order=None, count=False):
+        res = super().search(
+            domain, offset=offset, limit=limit, order=order, count=count
+        )
+        # if this is used to count, sorting does not make sense, also if limit is set
+        # at this point sorting for what?
+        if (
+            self.env.context.get("active_shipment_advice_id")
+            and not count
+            and not limit
+        ):
+            res = res.sorted("picking_sequence_in_shipment_advice")
         return res
