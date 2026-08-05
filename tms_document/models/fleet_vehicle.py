f90ce8a9dcc83a115095636bf348e49854678e53
Volkan TAŞCI <me@volkantasci.com>
2026-08-05 20:04:21 +0300

    [ADD] tms_document: computed document_ids O2M on driver and vehicle

---
+++ tms_document/models/fleet_vehicle.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2026 VSL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class FleetVehicle(models.Model):
+    _inherit = "fleet.vehicle"
+
+    document_ids = fields.One2many(
+        "tms.document", "res_id", string="Documents", compute="_compute_document_ids"
+    )
+
+    def _compute_document_ids(self):
+        Doc = self.env["tms.document"]
+        for rec in self:
+            rec.document_ids = Doc.search(
+                [("res_model", "=", "fleet.vehicle"), ("res_id", "=", rec.id)]
+            )
