d07b7137d151671299ef442a74181bd32fb2ed0d
Volkan TAŞCI <me@volkantasci.com>
2026-08-05 20:00:55 +0300

    [ADD] tms_document: document model with non-stored validity compute

---
+++ tms_document/tests/test_tms_document.py
@@ -0,0 +1,33 @@
+from datetime import date, timedelta
+
+from odoo import fields
+from odoo.tests.common import TransactionCase
+
+
+class TestTmsDocument(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.holder = cls.env["tms.driver"].create({"name": "Doc Holder"})
+        cls.Doc = cls.env["tms.document"]
+
+    def _doc(self, expiry):
+        return self.Doc.create({
+            "res_model": "tms.driver",
+            "res_id": self.holder.id,
+            "doc_type": "license",
+            "name": "LIC-1",
+            "expiry_date": expiry,
+        })
+
+    def test_state_valid(self):
+        d = self._doc(fields.Date.to_date(date.today()) + timedelta(days=400))
+        self.assertEqual(d.state, "valid")
+
+    def test_state_expired(self):
+        d = self._doc(fields.Date.to_date(date.today()) - timedelta(days=1))
+        self.assertEqual(d.state, "expired")
+
+    def test_state_expiring_within_horizon(self):
+        d = self._doc(fields.Date.to_date(date.today()) + timedelta(days=5))
+        self.assertEqual(d.state, "expiring")
