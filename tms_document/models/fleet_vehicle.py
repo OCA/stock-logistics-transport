05a181ecdde50e726c0be650d4f125a7358ac35e
Volkan TAŞCI <me@volkantasci.com>
2026-08-09 23:59:48 +0300

    [IMP] tms_document: allow editing documents on holder forms

--- tms_document/models/fleet_vehicle.py
+++ tms_document/models/fleet_vehicle.py
@@ -7,7 +7,12 @@ class FleetVehicle(models.Model):
     _inherit = "fleet.vehicle"
 
     document_ids = fields.One2many(
-        "tms.document", "res_id", string="Documents", compute="_compute_document_ids"
+        "tms.document",
+        "res_id",
+        string="Documents",
+        compute="_compute_document_ids",
+        inverse="_inverse_document_ids",
+        readonly=False,
     )
 
     def _compute_document_ids(self):
@@ -16,3 +21,9 @@ class FleetVehicle(models.Model):
             rec.document_ids = Doc.search(
                 [("res_model", "=", "fleet.vehicle"), ("res_id", "=", rec.id)]
             )
+
+    def _inverse_document_ids(self):
+        for rec in self:
+            for doc in rec.document_ids:
+                if doc.res_model != rec._name or doc.res_id != rec.id:
+                    doc.write({"res_model": rec._name, "res_id": rec.id})
