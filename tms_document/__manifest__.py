0b3b2dd43fc4c2ccbb6b6c03e0360c750c325247
Volkan TAŞCI <me@volkantasci.com>
2026-08-09 23:11:28 +0300

    [IMP] tms_document: per-row Delete button on holder Documents tab

--- tms_document/__manifest__.py
+++ tms_document/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "TMS Document",
     "summary": "Generic expiry-tracked document framework for TMS",
-    "version": "19.0.1.0.3",
+    "version": "19.0.1.0.4",
     "license": "AGPL-3",
     "category": "TMS",
     "author": "VSL, Odoo Community Association (OCA)",
